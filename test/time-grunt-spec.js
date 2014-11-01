29e34e91b7ed0720522c1f28047f90d25a488503
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2014-11-01 13:33:09 +0100

    #31: added time-grunt-spec.js

---
+++ test/time-grunt-spec.js
@@ -0,0 +1,46 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var fs = require('fs');
+var answers = require('../test_helpers/prompt-answer-factory')({
+	"installAssemble": false
+});
+
+
+describe('time-grunt', function () {
+	var helperPath = "helpers/";
+
+	beforeEach(function (done) {
+		helpers.testDirectory(path.join(__dirname, 'tmp'), function (err) {
+			if (err) {
+				return done(err);
+			}
+
+			this.app = helpers.createGenerator('prototype:app', [
+				'../../generators/app'
+			]);
+
+			helpers.mockPrompt(this.app, answers);
+			this.app.options['skip-install'] = true;
+			this.app.options['skip-welcome-message'] = true;
+
+			done();
+		}.bind(this));
+	});
+
+	it('adds references to package.json', function (done) {
+		this.app.run({}, function () {
+			helpers.assertFile('package.json', /time-grunt/);
+		});
+		done();
+	});
+
+	it('adds task to Gruntfile.js file', function (done) {
+		this.app.run({}, function () {
+			helpers.assertFile("Gruntfile.js", /\'time-grunt\'/);
+			done();
+		});
+	});
+});
