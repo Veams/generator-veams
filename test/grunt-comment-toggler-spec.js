76b85f69eeed31b60a64d2e2852c0029261e4986
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2014-11-01 13:40:00 +0100

    #31: added grunt-comment-toggler-spec.js

--- test/grunt-comment-toggler-spec.js
+++ test/grunt-comment-toggler-spec.js
@@ -5,13 +5,13 @@ var path = require('path');
 var helpers = require('yeoman-generator').test;
 var fs = require('fs');
 var answers = require('../test_helpers/prompt-answer-factory')({
-	"installAssemble": false,
-	"features": [
-		"mobileFirst"
+	"installAssemble": true,
+	"modules": [
+		"grunt-comment-toggler"
 	]
 });
 
-describe('grunt-comment-media-queries', function () {
+describe('grunt-comment-toggler', function () {
 	var helperPath = "helpers/";
 
 	beforeEach(function (done) {
@@ -34,21 +34,22 @@ describe('grunt-comment-media-queries', function () {
 
 	it('adds references to package.json', function (done) {
 		this.app.run({}, function () {
-			helpers.assertFile('package.json', /grunt-combine-media-queries/);
+			helpers.assertFile('package.json', /grunt-comment-toggler/);
 		});
 		done();
 	});
 
 	it('creates helper files', function (done) {
 		this.app.run({}, function () {
-			helpers.assertFile(helperPath + "_grunt/comment-media-queries.js");
+			helpers.assertFile(helperPath + "_grunt/toggleComments.js");
 			done();
 		});
 	});
 
 	it('adds task to Gruntfile.js file', function (done) {
 		this.app.run({}, function () {
-			helpers.assertFile("Gruntfile.js", /\'comment-media-queries:dist\'/);
+			helpers.assertFile("Gruntfile.js", /\'toggleComments\'/);
+			helpers.assertFile("Gruntfile.js", /toggleComments: \'grunt-comment-toggler\'/);
 			done();
 		});
 	});
