3c7b4b9405b4ea1762f864158395f8c22ec68a82
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2014-11-01 13:59:45 +0100

    #31: added grunt-contrib-copy-spec.js

---
+++ test/grunt-contrib-copy-spec.js
@@ -0,0 +1,49 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var fs = require('fs');
+var answers = require('../test_helpers/prompt-answer-factory')({
+	"installAssemble": false,
+	"features": [
+		"installDocs",
+		"createDevFolder"
+	]
+});
+
+describe('grunt-contrib-copy', function () {
+	var helperPath = "helpers/";
+
+	beforeEach(function (done) {
+		helpers.testDirectory(path.join(__dirname, 'tmp'), function (err) {
+			if (err) {
+				return done(err);
+			}
+
+			this.app = helpers.createGenerator('prototype:app', [
+				'../../generators/app'
+			]);
+
+			helpers.mockPrompt(this.app, answers);
+			this.app.options['skip-install'] = true;
+			this.app.options['skip-welcome-message'] = true;
+
+			done();
+		}.bind(this));
+	});
+
+	it('adds references to package.json', function (done) {
+		this.app.run({}, function () {
+			helpers.assertFile('package.json', /grunt-contrib-copy/);
+		});
+		done();
+	});
+
+	it('creates helper files', function (done) {
+		this.app.run({}, function () {
+			helpers.assertFile(helperPath + "_grunt/copy.js");
+			done();
+		});
+	});
+});
