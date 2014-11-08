a12e114c77424c7b905af05189e8420a2ecc8d6a
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2014-11-09 00:22:43 +0100

    #31: added grunt-devtools-spec.js

---
+++ test/grunt-devtools-spec.js
@@ -0,0 +1,42 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var fs = require('fs');
+var answers = require('../test_helpers/prompt-answer-factory')({
+	"modules": [
+		"grunt-devtools"
+	]
+});
+
+
+describe('grunt-devtools', function () {
+	var srcPath = "resources/";
+	var helperPath = "helpers/";
+
+	beforeEach(function (done) {
+		helpers.testDirectory(path.join(__dirname, 'tmp'), function (err) {
+			if (err) {
+				return done(err);
+			}
+
+			this.app = helpers.createGenerator('prototype:app', [
+				'../../generators/app'
+			]);
+
+			helpers.mockPrompt(this.app, answers);
+			this.app.options['skip-install'] = true;
+			this.app.options['skip-welcome-message'] = true;
+
+			done();
+		}.bind(this));
+	});
+
+	it('adds references to package.json', function (done) {
+		this.app.run({}, function () {
+			helpers.assertFile('package.json', /grunt-devtools/);
+		});
+		done();
+	});
+});
