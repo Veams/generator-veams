23b994c85b56cab73fc78eae36cef85290e10f62
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2014-11-09 00:24:53 +0100

    #31: added grunt-data-separator-spec.js

---
+++ test/grunt-data-separator-spec.js
@@ -0,0 +1,56 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var fs = require('fs');
+var answers = require('../test_helpers/prompt-answer-factory')({
+	"modules": [
+		"grunt-data-separator"
+	]
+});
+
+
+describe('grunt-data-separator', function () {
+	var srcPath = "resources/";
+	var helperPath = "helpers/";
+
+	beforeEach(function (done) {
+		helpers.testDirectory(path.join(__dirname, 'tmp'), function (err) {
+			if (err) {
+				return done(err);
+			}
+
+			this.app = helpers.createGenerator('prototype:app', [
+				'../../generators/app'
+			]);
+
+			helpers.mockPrompt(this.app, answers);
+			this.app.options['skip-install'] = true;
+			this.app.options['skip-welcome-message'] = true;
+
+			done();
+		}.bind(this));
+	});
+
+	it('adds references to package.json', function (done) {
+		this.app.run({}, function () {
+			helpers.assertFile('package.json', /grunt-data-separator/);
+		});
+		done();
+	});
+
+	it('creates helper files', function (done) {
+		this.app.run({}, function () {
+			helpers.assertFile(helperPath + "_grunt/dataSeparator.js");
+			done();
+		});
+	});
+
+	it('adds task to Gruntfile.js file', function (done) {
+		this.app.run({}, function () {
+			helpers.assertFile("Gruntfile.js", /\'dataSeparator\'/);
+			done();
+		});
+	});
+});
