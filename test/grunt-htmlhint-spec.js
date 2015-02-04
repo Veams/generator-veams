c5a9da53581febef86ed23949d8759a5862cfb83
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2015-02-04 20:37:16 +0100

    #31: added htmlhint-spec, changed fileindex-spec file name

---
+++ test/grunt-htmlhint-spec.js
@@ -0,0 +1,34 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var fs = require('fs');
+var answers = require('../test_helpers/prompt-answer-factory')({});
+
+describe('grunt-htmlhint', function () {
+	var helperPath = "helpers/";
+
+	beforeEach(function (done) {
+		helpers.run(path.join(__dirname, '../generators/app'))
+			.inDir(path.join(__dirname, 'tmp'))
+			.withOptions({
+				'skip-install': true,
+				'skip-welcome-message': true
+			})
+			.withPrompts(answers)
+			.on('end', done);
+	});
+
+	it('adds references to package.json', function () {
+		helpers.assertFile('package.json', /grunt-htmlhint/);
+	});
+
+	it('creates helper files', function () {
+		helpers.assertFile(helperPath + "_grunt/htmlhint.js");
+	});
+
+	it('adds task to Gruntfile.js file', function () {
+		helpers.assertFile("Gruntfile.js", /\'htmlhint\'/);
+	});
+});
