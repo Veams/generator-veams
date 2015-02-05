d94c184097aa9417e3aa632fc90c0141cac4f853
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2015-02-05 17:27:00 +0100

    #31: added version spec

---
+++ test/grunt-version-spec.js
@@ -0,0 +1,48 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-generator').assert;
+var fs = require('fs');
+var answers = require('../test_helpers/prompt-answer-factory')({
+	"modules":[
+		"grunt-version"
+	],
+	"features": [
+		"createDevFolder"
+	]
+});
+
+describe('grunt-version', function () {
+	var helperPath = "helpers/";
+	var srcPath = "resources/";
+
+	beforeEach(function (done) {
+		helpers.run(path.join(__dirname, '../generators/app'))
+			.inDir(path.join(__dirname, 'tmp'))
+			.withOptions({
+				'skip-install': true,
+				'skip-welcome-message': true
+			})
+			.withPrompts(answers)
+			.on('end', done);
+	});
+
+	it('adds references to package.json', function () {
+		assert.fileContent('package.json', /grunt-version/);
+	});
+
+	it('creates helper files', function () {
+		assert.file(helperPath + "_grunt/version.js");
+	});
+
+	it('creates partial file', function () {
+		assert.file(srcPath + "templates/partials/blocks/b-version.hbs");
+	});
+
+	it('adds task to Gruntfile.js file', function () {
+		assert.fileContent("Gruntfile.js", /version/);
+	});
+
+});
