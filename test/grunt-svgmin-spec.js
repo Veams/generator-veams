381c14b20eadb44e6b0e80bb0763ef8eb51caed5
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2015-02-05 17:21:19 +0100

    added svgmin spec

---
+++ test/grunt-svgmin-spec.js
@@ -0,0 +1,41 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-generator').assert;
+var fs = require('fs');
+var answers = require('../test_helpers/prompt-answer-factory')({
+	"modules":[
+		"grunt-svgmin",
+		"grunt-grunticon"
+	]
+});
+
+describe('grunt-svgmin', function () {
+	var helperPath = "helpers/";
+
+	beforeEach(function (done) {
+		helpers.run(path.join(__dirname, '../generators/app'))
+			.inDir(path.join(__dirname, 'tmp'))
+			.withOptions({
+				'skip-install': true,
+				'skip-welcome-message': true
+			})
+			.withPrompts(answers)
+			.on('end', done);
+	});
+
+	it('adds references to package.json', function () {
+		assert.fileContent('package.json', /grunt-svgmin/);
+	});
+
+	it('creates helper files', function () {
+		assert.file(helperPath + "_grunt/svgmin.js");
+	});
+
+	it('adds task to Gruntfile.js file', function () {
+		assert.fileContent("Gruntfile.js", /\'svgmin\'/);
+	});
+
+});
