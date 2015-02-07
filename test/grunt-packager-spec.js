1b516d33f6bd1b469f058e8d21eca17a63d82e7f
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2015-02-07 22:31:51 +0100

    #31: added packager spec

---
+++ test/grunt-packager-spec.js
@@ -0,0 +1,40 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-generator').assert;
+var fs = require('fs');
+var answers = require('../test_helpers/prompt-answer-factory')({
+	"modules":[
+		"grunt-packager"
+	]
+});
+
+describe('grunt-packager', function () {
+	var helperPath = "helpers/";
+	var srcPath = "resources/";
+
+	beforeEach(function (done) {
+		helpers.run(path.join(__dirname, '../generators/app'))
+			.inDir(path.join(__dirname, 'tmp'))
+			.withOptions({
+				'skip-install': true,
+				'skip-welcome-message': true
+			})
+			.withPrompts(answers)
+			.on('end', done);
+	});
+
+	it('adds references to package.json', function () {
+		assert.fileContent('package.json', /grunt-packager/);
+	});
+
+	it('creates helper files', function () {
+		assert.file(helperPath + "_grunt/packager.js");
+	});
+
+	it('creates config file', function () {
+		assert.file(srcPath + "js/project.jspackcfg");
+	});
+});
