bfc3466d16af7e4e8a245f70740c71fd3022e693
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2015-07-29 16:56:41 +0200

    added config file for paths

---
+++ generator-files/config.js
@@ -0,0 +1,9 @@
+var config = module.exports;
+
+config.paths = {
+	appPath: 'app/templates/',
+	srcPath: 'resources/',
+	helperPath: 'helpers/',
+	gruntPath: 'helpers/_grunt/',
+	gulpPath: 'helpers/_gulp/'
+};
