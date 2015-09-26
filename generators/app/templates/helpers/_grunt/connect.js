c36348ca04a18d3f35d5b47d732294b31ef6d983
Sebastian Fitzner <sebastian@jci-werbeagentur.com>
2015-09-26 13:45:03 +0200

    deleted packager and connect

--- generators/app/templates/helpers/_grunt/connect.js
+++ generators/app/templates/helpers/_grunt/connect.js
@@ -1,47 +1,16 @@
 module.exports = {
-    options: {
-        port: '<%%= ports.app %>',
-        livereload: '<%%= ports.livereload %>',
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-    },
-    livereload: {
-        options: {
-            open: true,
-            base: [
-                '<%%= paths.dev %>'
-            ]
-        }
-    }<% if(gruntModules && gruntModules.length > 0 && gruntModules.indexOf('grunt-connect-proxy') != -1 && proxyHost && proxyPort) { %>,
-    proxy: {
-        options: {
-            hostname: '0.0.0.0',
-            port: 8000,
-            middleware: function(connect, options, next) {
-                var proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
-                var proxySnippetWithHeader = function (req, res, next) {
-                    // allow CORS for dev proxy
-                    res.setHeader('Access-Control-Allow-Origin', '*');
-                    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,HEAD');
-                    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-                    return proxySnippet(req, res, next);
-                };
-                return [proxySnippetWithHeader];
-            }
-        },
-        proxies: [
-        {
-            context: '/',
-            host: '<%= proxyHost %>',
-            port: <%= proxyPort %>
-            // https: true,
-            // headers: {
-            //  // example config for basic authentification
-            //  'Authorization': 'Basic YmFua19vZl9zY290X2VsYnJpbms6VGVzdC1hcGVydG8yMDEz',
-            //  'Accept': 'application/json',
-            //  'Access-Control-Allow-Origin': '127.0.0.1:9001'
-            // }         
-        }]
-    }<% } %>
+	options: {
+		port: '<%%= ports.app %>',
+		livereload: '<%%= ports.livereload %>',
+		// change this to '0.0.0.0' to access the server from outside
+		hostname: 'localhost'
+	},
+	livereload: {
+		options: {
+			open: true,
+			base: [
+				'<%%= paths.dev %>'
+			]
+		}
+	}
 };
--- generators/app/templates/helpers/_grunt/packager.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	default: {
-		options: {
-			config: '<%= paths.src %>/js/project.jspackcfg',
-			cwd: '<%= paths.src %>/js/'
-		}
-	}
-};
