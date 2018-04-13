d359e4f3ccd5751f3479ee04e14e5d37d9ee4fbc
Sebastian Fitzner <sebastian.fitzner@aperto.com>
2018-04-13 10:49:13 +0200

    Fix command-line-usage package issue

--- mini-generators/css-frameworks/scaffold.js
+++ mini-generators/css-frameworks/scaffold.js
@@ -2,16 +2,16 @@ const config = require('./config');
 
 module.exports = function scaffold() {
 	// Delete CSS packages
-	if (this.cssLibs.indexOf(config.foundationId) == -1) delete this.pkgFile['devDependencies']['foundation-sites'];
-	if (this.cssLibs.indexOf(config.includeMediaId) == -1) delete this.pkgFile['devDependencies']['include-media'];
-	if (this.cssLibs.indexOf(config.bootstrapId) == -1) delete this.pkgFile['dependencies']['bootstrap-sass'];
-	if (this.cssLibs.indexOf(config.neatId) == -1) {
+	if (this.cssLibs.indexOf(config.foundationId) === -1) delete this.pkgFile['devDependencies']['foundation-sites'];
+	if (this.cssLibs.indexOf(config.includeMediaId) === -1) delete this.pkgFile['devDependencies']['include-media'];
+	if (this.cssLibs.indexOf(config.bootstrapId) === -1) delete this.pkgFile['dependencies']['bootstrap-sass'];
+	if (this.cssLibs.indexOf(config.neatId) === -1) {
 		delete this.pkgFile['devDependencies']['bourbon-neat'];
 		delete this.pkgFile['devDependencies']['neat'];
 	}
 
 	// Grunt handling
-	if (this.cssLibs.indexOf(config.lostGridId) != -1) {
+	if (this.cssLibs.indexOf(config.lostGridId) !== -1) {
 		if (this.taskRunner.indexOf('grunt') !== -1) {
 			this.fs.copyTpl(
 				this.templatePath(this.generatorGruntPath + 'postcss.js.ejs'),
